b538a48c311fc9262a85bc92989f62756aec008c
youshamahmood <youshamahmood96@gmail.com>
2021-03-16 11:27:45 +0600

    first commit

---
+++ src/redux/reducers/dummyReducer.js
@@ -0,0 +1,3 @@
+export const dummyReducer =(state,action) =>{
+    return state
+}
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,4 @@
+import {combineReducers} from 'redux'
+import {dummyReducer} from './dummyReducer'
+const rootReducer = combineReducers({dummyReducer})
+export default rootReducer
